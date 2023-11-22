5dab087f79b2cd2aadd0f5b990030c5ba0e2a315
hast06001 <hast06001@E-PW024NW1>
2023-11-22 12:45:17 +0100

    Add project files.

---
+++ Uppgift 5.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift_5._2
+{
+    class Porgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal vill du skriva in?");
+            int antal = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Skriv in {antal} heltal. (ha ett mellanslag mellan varje tal.");
+            string talenText = Console.ReadLine();
+            string[] talText = talenText.Split(' ');
+
+            for (int i = 0; i < antal; i++) 
+            {
+                Console.WriteLine($"Tal {i + 1}: {talText[i]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
